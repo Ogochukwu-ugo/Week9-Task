f9b22fcc629762ccfd8d8b68550d4e5e0bc0c443
Ogochukwu Ugorji <ugorji.ogochukwu@gmail.com>
2022-12-18 05:56:29 +0100

    Corrected form validation

--- login.js
+++ login.js
@@ -4,10 +4,57 @@ const password = document.getElementById('password');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    if(username == " " || password == " ") {
-        alert("please enter your username or password")
+    if(username.value.trim() === "" || password.value.trim() === "") {
+        alert("Please enter your username or password");
      }
      else {
-        alert("Success")
+        alert("Success");
+        window.location.href = '/success.html'
     }
-})
+});
+
+
+
+
+
+
+
+
+
+// function validate() {
+//     // Get form elements
+//     var username = document.getElementById("username");
+//     var password = document.getElementById("password");
+
+//     // Validate form inputs
+//     if (username.value.trim() === "" || password.value.trim() ==="") {
+//         alert("Please enter a username and password");
+//         return false;
+//     }
+
+//     // Return true if form is valid
+//     else{
+//         alert("Success!!!");
+
+//     }
+//     return true;
+// }
+// document.getElementById("form-detail").onsubmit = validate;
+
+
+// function validate() {
+//     var userName = document.getElementById("username");
+//     var password = document.getElementById("password");
+
+//     if(userName.value.trim() === "" || password.value.trim() === "")
+//     {
+//         alert( "Enter username or password");
+//         return false;
+//     }
+//     if(userName.value.length > 4 && password.value.length >= 6)
+//     {
+//         alert("Success!");
+//         return true;
+//     }
+// }
+
